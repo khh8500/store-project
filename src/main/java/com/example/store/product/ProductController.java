903c4b9985fe426226dfc0aa6920df12bba0f07d
khh <khh8500@naver.com>
2024-04-24 10:00:02 +0900

    상품 목록보기

--- src/main/java/com/example/store/product/ProductController.java
+++ src/main/java/com/example/store/product/ProductController.java
@@ -1,17 +1,25 @@
 package com.example.store.product;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
     @GetMapping("/product")
-    public String list(){
+    public String list(HttpServletRequest request){
+        List<Product> productList = productService.findAll();
+        request.setAttribute("productList", productList);
+
         return "/product/list";
     }
 
---
+++ src/main/java/com/example/store/product/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.store.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
